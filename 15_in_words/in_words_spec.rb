4dd8135b73124242e3f43f97b852d8816ef6db6d
Brandon Closson <bclosson@gmail.com>
2014-04-13 17:11:45 -0400

    got tests to pass on 15_in_words, needs refactoring

--- 15_in_words/in_words_spec.rb
+++ 15_in_words/in_words_spec.rb
@@ -6,7 +6,8 @@
 #
 # # Extending a built-in class
 #
-# To make this test pass you'll need to extend a built-in class. You'll see that we're creating a new spec for 'Fixnum' -- this isn't a new class you'll be building, but instead it is a built-in class you will extend.
+# To make this test pass you'll need to extend a built-in class. You'll see that we're creating a 
+# new spec for 'Fixnum' -- this isn't a new class you'll be building, but instead it is a built-in class you will extend.
 #
 # Remember that in Ruby, everything is an object, even a number.  Try this in irb:
 #
@@ -15,7 +16,8 @@
 #     >> 4.methods
 #     \["inspect", "%", "<<", ...
 #
-# The number 4 is of class `FixNum` and it has methods on it.  Your challenge is to add an `in_words` method to `FixNum`.
+# The number 4 is of class `FixNum` and it has methods on it.  Your challenge is to add an `in_words` 
+# method to `FixNum`.
 #
 
 require "in_words"
